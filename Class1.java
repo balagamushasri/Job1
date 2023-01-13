93b11e6095863f3c457cb08b59c979657d98f361
ushasri <ushasribalagam@gmail.com>
2023-01-13 18:16:48 +0530

    first commit

---
+++ Class1.java
@@ -0,0 +1,20 @@
+package Package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class1 p1 = new Class1();
+
+		p1.program1();
+
+		}
+
+		public void program1()
+
+		{
+
+		System.out.println("Simple Java program");
+	}
+
+}
